1655ad83ae7ee76a9a3d289a2d9293a4b6efc9a1
AliAftab0786 <meharaliafatb0306@gmail.com>
2024-07-14 02:55:22 -0700

    All Code updated again.

--- app.py
+++ app.py
@@ -52,5 +52,4 @@ def update_task(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0',debug=False)
+    app.run(debug=True)
