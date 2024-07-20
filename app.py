15c9cab2b15833d0fecfbd6783426803275d423a
AliAftab0786 <meharaliafatb0306@gmail.com>
2024-07-20 18:34:05 +0500

    Update Backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify , render_template , redirect , url_for 
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__ , template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/todo'
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class tasks(db.Model):
@@ -15,17 +17,20 @@ class tasks(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html' , tasks = tasks.query.all())
+    return render_template('index.html', tasks=tasks.query.all())
 
 @app.route('/add', methods=['POST'])
 def add():
     task_name = request.form['name']
     description = request.form['description']
-    due_date = request.form['due_date']
-    new_task = tasks(task_name=task_name, description=description , due_date=due_date)
+    due_date_str = request.form['due_date']
+    # Convert the due_date string to a datetime object
+    due_date = datetime.strptime(due_date_str, '%Y-%m-%d')
+    new_task = tasks(task_name=task_name, description=description, due_date=due_date)
     db.session.add(new_task)
     db.session.commit()
     return redirect(url_for('index'))
+
 @app.route('/delete', methods=['POST'])
 def delete_task():
     if request.form['_method'] == 'DELETE':
@@ -34,6 +39,7 @@ def delete_task():
         db.session.delete(task)
         db.session.commit()
     return redirect(url_for('index'))
+
 @app.route('/edit/<int:task_id>', methods=['GET'])
 def edit_task(task_id):
     task = tasks.query.filter_by(task_id=task_id).first()
@@ -41,15 +47,20 @@ def edit_task(task_id):
         return render_template('edit.html', task=task)
     else:
         return redirect(url_for('index'))
+
 @app.route('/update/<int:task_id>', methods=['POST'])
 def update_task(task_id):
     task = tasks.query.filter_by(task_id=task_id).first()
     if task:
         task.task_name = request.form['task_name']
         task.description = request.form['description']
-        task.due_date = request.form['due_date']
+        due_date_str = request.form['due_date']
+        # Convert the due_date string to a datetime object
+        task.due_date = datetime.strptime(due_date_str, '%Y-%m-%d')
         db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True , port=5001)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5001)
